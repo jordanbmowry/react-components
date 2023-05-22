530e9ee5f21fe75727ad52b39f1530e75a3090d8
Joran Mowry <jordan.mowry@gmail.com>
2023-05-22 15:19:53 -0500

    retyped button

---
+++ src/pages/ButtonPage.tsx
@@ -0,0 +1,40 @@
+import Button from '../components/Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
+function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button
+          className='mb-2'
+          onClick={() => console.log('click')}
+          rounded
+          primary
+        >
+          <GoBell />
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger>
+          <GoCloudDownload />
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button outline warning>
+          <GoDatabase />
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary>Hide Ads!</Button>
+      </div>
+      <div>
+        <Button success>Something!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
